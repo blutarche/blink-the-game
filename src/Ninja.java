a650b2038b6c288c783b164e6da39f250e4324a3
blutarche <visitor19blut@gmail.com>
2014-09-15 11:38:31 +0700

    Ninja is now able to turn around!

--- src/Ninja.java
+++ src/Ninja.java
@@ -16,33 +16,33 @@ public class Ninja {
 	private static final int CHR_WIDTH 	= 40;
 	private static final int CHR_HEIGHT = 40;
 	
-	  private boolean left;	private boolean right;
-	  private boolean up;	private boolean down;
-	
+	private boolean left;	private boolean right;
+	private boolean up;		private boolean down;
+	 
 	public Ninja(float x, float y) throws SlickException {
 	    this.x = x;
 	    this.y = y;
 	    this.v = 4;
 	    image = new Image("res/ninja-dot.png");
-		image.setCenterOfRotation(CHR_WIDTH/2, CHR_HEIGHT/2);
-	  }
+	}
 
-	  public void render() {
-	    image.draw(x - (CHR_WIDTH/2), y - (CHR_HEIGHT/2), CHR_WIDTH, CHR_HEIGHT);
+	public void render() {
+	    image.draw(x, y, CHR_WIDTH, CHR_HEIGHT);
 	    adjustHeadDirection();
-	  }
+	}
 
-		private void adjustHeadDirection () {
-			int mouseX = BlinkTheGame.mouseX;
-			int mouseY = BlinkTheGame.mouseY;
-			float xDistance = mouseX - x;
-			float yDistance = mouseY - y;
-			double angleToTurn = Math.toDegrees(Math.atan2(yDistance, xDistance));
-			image.setRotation((float)angleToTurn);
-		}
-	  public void update(GameContainer container) {
+	private void adjustHeadDirection () {
+		float mouseX = (float)BlinkTheGame.mouseX;
+		float mouseY = (float)BlinkTheGame.mouseY;
+		float xDistance = mouseX - x;
+		float yDistance = mouseY - y;
+		double angleToTurn = Math.toDegrees(Math.atan2(yDistance, xDistance));
+		image.setRotation((float)angleToTurn);
+	}
+	public void update(GameContainer container) {
 	    Input input = container.getInput();
 		left=false; right=false; up=false; down=false;
+		image.setCenterOfRotation(CHR_WIDTH/2, CHR_HEIGHT/2);
 	    if (input.isKeyDown(Input.KEY_A)) { 
 	        left = true;
 	    }
@@ -57,39 +57,39 @@ public class Ninja {
 	    }
 	    changePosition();
 	    adjustPosition();
-	  }
-		private void changePosition () {
-			if (left) {
-				if (up) {
-					x-=v;
-					y-=v;
-				}
-				else if (down) {
-					x-=v;
-					y+=v;
-				}
-				else x-=v*Math.sqrt(2);
+	}
+	private void changePosition () {
+		if (left) {
+			if (up) {
+				x-=v;
+				y-=v;
 			}
-			else if (right) {
-				if (up) {
-					x+=v;
-					y-=v;
-				}
-				else if (down) {
-					x+=v;
-					y+=v;
-				}
-				else x+=v*Math.sqrt(2);
+			else if (down) {
+				x-=v;
+				y+=v;
 			}
-			else if (up) y-=v*Math.sqrt(2);
-			else if (down) y+=v*Math.sqrt(2);
+			else x-=v*Math.sqrt(2);
 		}
-		private void adjustPosition () {
-			int limitX = BlinkTheGame.GAME_WIDTH - CHR_WIDTH;
-			int limitY = BlinkTheGame.GAME_HEIGHT - CHR_HEIGHT;
-			if (x<CHR_WIDTH) x=CHR_WIDTH;
-			if (x>limitX) x=limitX;
-			if (y<CHR_HEIGHT) y=CHR_HEIGHT;
-			if (y>limitY) y=limitY;
+		else if (right) {
+			if (up) {
+				x+=v;
+				y-=v;
+			}
+			else if (down) {
+				x+=v;
+				y+=v;
+			}
+			else x+=v*Math.sqrt(2);
 		}
+		else if (up) y-=v*Math.sqrt(2);
+		else if (down) y+=v*Math.sqrt(2);
+	}
+	private void adjustPosition () {
+		int limitX = BlinkTheGame.GAME_WIDTH - CHR_WIDTH;
+		int limitY = BlinkTheGame.GAME_HEIGHT - CHR_HEIGHT;
+		if (x<0) x=0;
+		if (x>limitX) x=limitX;
+		if (y<0) y=0;
+		if (y>limitY) y=limitY;
+	}
 }
