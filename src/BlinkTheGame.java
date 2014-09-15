f44a048efb0e22effce6ff77b426e0d3ddec8553
blutarche <visitor19blut@gmail.com>
2014-09-15 09:46:35 +0700

    Ninja appears on screen.

--- src/BlinkTheGame.java
+++ src/BlinkTheGame.java
@@ -1,5 +1,6 @@
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
@@ -7,14 +8,16 @@ import org.newdawn.slick.SlickException;
 
 public class BlinkTheGame extends BasicGame {
 
-	public static final int GAME_WIDTH 	= 800;
-	public static final int GAME_HEIGHT = 600;
+	public static final int GAME_WIDTH 	= 1366;
+	public static final int GAME_HEIGHT = 768;
+	
+	private Ninja ninja;
 	
 	public static void main (String [] args) {
 	    try {
 		      BlinkTheGame game = new BlinkTheGame("Blink!");
 		      AppGameContainer appgc = new AppGameContainer(game);
-		      appgc.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
+		      appgc.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, true);
 		      appgc.setMinimumLogicUpdateInterval(10);
 		      appgc.setMaximumLogicUpdateInterval(10);
 		      appgc.start();
@@ -29,19 +32,24 @@ public class BlinkTheGame extends BasicGame {
 	}
 
 	@Override
-	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+	public void render(GameContainer container, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
+		
+		ninja.render();
 		
 	}
 
 	@Override
-	public void init(GameContainer arg0) throws SlickException {
+	public void init(GameContainer container) throws SlickException {
 		// TODO Auto-generated method stub
+	    Color background = new Color(41, 51, 61);
+	    container.getGraphics().setBackground(background);        
+	    ninja = new Ninja(GAME_WIDTH/2,GAME_HEIGHT/2);
 		
 	}
 
 	@Override
-	public void update(GameContainer arg0, int arg1) throws SlickException {
+	public void update(GameContainer container, int val) throws SlickException {
 		// TODO Auto-generated method stub
 		
 	}
--- src/Ninja.java
+++ src/Ninja.java
@@ -1,4 +1,23 @@
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+
 
 public class Ninja {
+	private Image image;
+	
+	public static float x;
+	public static float y;
+	private static final int CHR_WIDTH 	= 80;
+	private static final int CHR_HEIGHT = 80;
+	
+	public Ninja(float x, float y) throws SlickException {
+	    this.x = x;
+	    this.y = y;
+	    image = new Image("res/ninja-dot.png");
+	  }
 
+	  public void render() {
+	    image.draw(x - (CHR_WIDTH/2), y - (CHR_HEIGHT/2), CHR_WIDTH, CHR_HEIGHT);
+	  }
 }
