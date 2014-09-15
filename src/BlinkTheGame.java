322cc4a635c262acacc474db5858a24d390453f1
blutarche <visitor19blut@gmail.com>
2014-09-15 09:27:06 +0700

    Add .gitignore

--- src/BlinkTheGame.java
+++ src/BlinkTheGame.java
@@ -1,4 +1,49 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-public class BlinkTheGame {
+
+public class BlinkTheGame extends BasicGame {
+
+	public static final int GAME_WIDTH 	= 800;
+	public static final int GAME_HEIGHT = 600;
+	
+	public static void main (String [] args) {
+	    try {
+		      BlinkTheGame game = new BlinkTheGame("Blink!");
+		      AppGameContainer appgc = new AppGameContainer(game);
+		      appgc.setDisplayMode(GAME_WIDTH, GAME_HEIGHT, false);
+		      appgc.setMinimumLogicUpdateInterval(10);
+		      appgc.setMaximumLogicUpdateInterval(10);
+		      appgc.start();
+		    } catch (SlickException e) {
+		      e.printStackTrace();
+		    }
+	}
+	
+	public BlinkTheGame(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(GameContainer arg0) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, int arg1) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
