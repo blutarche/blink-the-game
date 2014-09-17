9003f535c861ce80d4f7e05f0dca7bfd9413bdbf
blutarche <visitor19blut@gmail.com>
2014-09-17 17:56:48 +0700

    Stupid One run inside screen.

--- src/StupidOne.java
+++ src/StupidOne.java
@@ -7,11 +7,12 @@ public class StupidOne extends Enemy {
 	private double turningDegree;
 	private double distanceGoing;
 
-	private static final double RUN_LIMIT = 120.0;
+	private double RUN_LIMIT = 120.0;
+	private final double RUN_INTO_LIMIT = 180.0;
 	private static final double DEG_LIMIT = 120.0;
 	private static final double TURN_SPEED = 3.0;
 	
-	private int movementMode; //0=start 1=running 2=turnstart 3=turning
+	private int movementMode; //0=start 1=runintoscreen 2=running 3=turnstart 4=turning
 	
 	public StupidOne(float x, float y) throws SlickException {
 		super(x, y, 1.0f, "res/stupid-dot.png");
@@ -23,21 +24,46 @@ public class StupidOne extends Enemy {
 
 	public void update () {
 		if (distanceGoing >= RUN_LIMIT) {
-			movementMode = 2;
+			movementMode = 3;
 			distanceGoing = 0;
+			RUN_LIMIT = RUN_INTO_LIMIT;
 		}
 		if (movementMode == 0) {
 			checkIfInsideScreen();
 		}
+		else if (isOffScreen()) {
+			turnAround();
+		}
 		else if (movementMode == 1) {
+			RUN_LIMIT=240;
+			movementMode = 2;
 		}
 		else if (movementMode == 2) {
+		}
+		else if (movementMode == 3) {
 			turnStart();
 		}
 		else {
 			turning();
 		}
 		run();
+	}
+	
+	private void turnAround () {
+		System.out.println("eiei");
+		desiredTurnDegree = 180;
+		turningDegree = 0;
+		distanceGoing = 0;
+		movementMode = 0;
+	}
+	
+	private boolean isOffScreen () {
+		if (0<=x&&x<=BlinkTheGame.GAME_WIDTH-CHR_WIDTH && 0<=y&&y<=BlinkTheGame.GAME_HEIGHT-CHR_HEIGHT) {
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
 	
 	private void checkIfInsideScreen () {
@@ -70,13 +96,11 @@ public class StupidOne extends Enemy {
 	}
 	
 	private void turnStart () {
-		
 		double degreeChange = Math.random() * DEG_LIMIT * 2 - DEG_LIMIT;
 		int tempDegree = (int)degreeChange/(int)TURN_SPEED;
-		System.out.println("tempDegree:"+tempDegree);
 		this.desiredTurnDegree = tempDegree * TURN_SPEED;
 		this.turningDegree = 0.0;
-		movementMode = 3;
+		movementMode = 4;
 	}
 	
 	private void turning () {
@@ -84,7 +108,7 @@ public class StupidOne extends Enemy {
 		if (turningDegree==desiredTurnDegree) {
 			this.turningDegree = 0.0;
 			distanceGoing = 0;
-			movementMode = 1;
+			movementMode = 2;
 		}
 		else if (desiredTurnDegree<0) {
 			this.degree -= TURN_SPEED;
