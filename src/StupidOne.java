d5d4e5161f6679cecab73ef38647f4701f40f453
blutarche <visitor19blut@gmail.com>
2014-09-17 13:53:43 +0700

    Refactored a little bit.

--- src/StupidOne.java
+++ src/StupidOne.java
@@ -3,16 +3,21 @@ import org.newdawn.slick.SlickException;
 
 public class StupidOne extends Enemy {
 
-	private static final double DEG_LIMIT = 3.0;
+	private static final double DEG_LIMIT = 10.0;
 	
 	public StupidOne(float x, float y) throws SlickException {
-		super(x, y, 2.5f, "res/stupid-dot.png");
+		super(x, y, 2.0f, "res/stupid-dot.png");
 		// TODO Auto-generated constructor stub
 	}
 
 	public void update () {
 		double degreeChange = Math.random() * DEG_LIMIT * 2 - DEG_LIMIT;
 		this.degree += degreeChange;
+		double radians = Math.toRadians(this.degree);
+		double cos = Math.cos(radians);
+		double sin = Math.sin(radians);
+		this.x += this.v*cos;
+		this.y += this.v*sin;
 	}
 	
 	
